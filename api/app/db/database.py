419feefcb5965f42c3607528f766c103c0225bbd
kcl <kcl@vbox.myguest.virtualbox.org>
2025-07-17 07:20:44 +0200

    opt consideration in database.py and 1000 second cache leniency

--- api/app/db/database.py
+++ api/app/db/database.py
@@ -2,36 +2,53 @@ import sqlite3
 import os
 from joblib import Memory
 from collections import defaultdict
-from pprint import pprint
 
 memory = Memory("./cache")
 
 def get_connection(db_path: str = "database.db") -> sqlite3.Connection:
     return sqlite3.connect(db_path)
 
-def get_db_timestamp(db_path: str = "database.db") -> float:
+def get_db_timestamp(db_path: str = "database.db") -> int:
     """
     The purpose of this function is to invalidate cache by checking database updates.
     If the message pool limit is low, this function is generally needless.
+    
+    Heavy(ier) functions will use get_db_timestamp if and only if it uses @memory.cache
     """
 
-    return os.path.getmtime(db_path)
+    return int(os.path.getmtime(db_path)) // 1000 # We add a 1000 second leniency
 
-@memory.cache
-def get_nicks_with_x_plus_messages(x: int, db_time: float, db_path: str = "database.db") -> list[str]:
+def get_nicks_with_x_plus_messages(x: int, db_path: str = "database.db") -> list[str]:
     with sqlite3.connect(db_path) as conn:
-        res = conn.execute("SELECT nick FROM messages GROUP BY nick HAVING COUNT(nick) > ?", (x,))
+        res = conn.execute("SELECT nick FROM messages GROUP BY nick HAVING COUNT(*) > ?", (x,))
         return [u[0] for u in res]
     
 @memory.cache
-def get_messages_with_x_plus_messages(x: int, db_time: float, db_path: str = "database.db") -> dict[str, list[str]]:
+def get_messages_with_x_plus_messages(x: int, db_time: int, db_path: str = "database.db") -> dict[str, list[str]]:
     author_message = defaultdict(list)
     with sqlite3.connect(db_path) as conn:
-        res = conn.execute("SELECT nick,message FROM messages WHERE nick IN (SELECT nick FROM messages GROUP BY nick HAVING COUNT(nick) > ?)", (x,))
+        res = conn.execute("""SELECT m.nick, m.message 
+                           FROM messages m
+                           JOIN users u ON m.nick = u.nick
+                           WHERE opt = 1
+                           AND m.nick
+                            IN (SELECT nick 
+                            FROM messages
+                            GROUP BY nick
+                            HAVING COUNT(*) > ?)
+                           """, (x,))
         for nick, message in res:
             author_message[nick].append(message)
     
     return author_message
 
-db = get_connection()
-pprint(get_messages_with_x_plus_messages(150, get_db_timestamp()))
+def is_nick_eligible(count: int, nick: str, db_path: str = "database.db") -> bool:
+    with sqlite3.connect(db_path) as conn:
+        res = conn.execute("""SELECT
+                           COUNT(*)
+                           FROM messages m
+                           JOIN users u WHERE u.nick = m.nick
+                           AND opt = 1
+                           AND m.nick = (?)
+                           """, (nick,))
+        return int(res.fetchone()[0]) >= count
